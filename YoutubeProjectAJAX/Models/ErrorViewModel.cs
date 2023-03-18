fe2f1111492898138fbc647da32b1a2f6d7336ad
BurakAyan97 <burakayan97@outlook.com>
2023-03-18 23:37:13 +0300

    Add project files.

---
+++ YoutubeProjectAJAX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YoutubeProjectAJAX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
