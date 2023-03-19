af72a6c456315fbdb43d9355fd20621a358b766a
Burak Ayan <120954647+BurakAyan97@users.noreply.github.com>
2023-03-19 18:14:17 +0300

    Delete YoutubeProjectAJAX directory

--- YoutubeProjectAJAX/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace YoutubeProjectAJAX.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
